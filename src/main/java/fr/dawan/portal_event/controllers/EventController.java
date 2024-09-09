d7bb1057315670198ba8f0d942493d6fca9f755b
Enzo Taurines <enzodylan.taurines@gmail.com>
2024-09-09 16:57:33 +0200

    Event might be added now: need to work on spring security to try it correctly

--- src/main/java/fr/dawan/portal_event/controllers/EventController.java
+++ src/main/java/fr/dawan/portal_event/controllers/EventController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,11 @@ public class EventController {
     public ResponseEntity<List<Event>> getAllEvents(){
             List<Event> events = eventService.getAllEvents();
         return new ResponseEntity<>(events, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event){
+        Event createdEvent = eventService.createEvent(event);
+        return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
     }
 }
