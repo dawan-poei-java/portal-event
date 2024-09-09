3368208f83c3e314195797b48bb64e014a488648
Enzo Taurines <enzodylan.taurines@gmail.com>
2024-09-09 11:59:02 +0200

    First commit

---
+++ src/main/java/fr/dawan/portal_event/PortalEventApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.portal_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/portal_event/PortalEventApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.portal_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
